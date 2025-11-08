ef376328fd0dbe57bca76f7db742ab6500f85ab9
Maram berrahal <berrahalmaram5@gmail.com>
2025-11-08 13:45:00 +0100

    here i change somthing

--- router/AppRouter.js
+++ router/AppRouter.js
@@ -2,8 +2,19 @@ import {Router} from "express";
 
 const appRouter = Router();
 
-appRouter.get("/create",(req,res)=> {
+appRouter.get("/note",(req,res)=> {
+    res.render("auth/note");
+})
+appRouter.get("/addNote",(req,res)=> {
     res.render("auth/addNote");
 })
-
+appRouter.get("/AllNote",(req,res)=> {
+    res.render("auth/AllNotes");
+})
+appRouter.get("/getANote",(req,res)=> {
+    res.render("auth/getANote");
+})
+appRouter.get("/delete",(req,res)=> {
+    res.render("auth/deleteANote");
+})
 export default appRouter;
